ed67fc37e989dbf734d08c075c2e1582aa742f99
Akanksha-Gawade <gawadeakanksha950@gmail.com>
2025-10-03 08:33:36 +0530

    Add files via upload

---
+++ addtion.java
@@ -0,0 +1,15 @@
+
+public class addtion {
+    public static void main(String[] aregs){
+        int a=30;
+        int b=20;
+    int c = a + b;
+    int d = a - b;
+    int e = a * b;
+    
+    System.out.println("addtion of a and b" + c);
+    System.out.println ("addtion of a and b" + d);
+    System.out.println("ddtion of a and b"+ e);
+
+    }
+}
